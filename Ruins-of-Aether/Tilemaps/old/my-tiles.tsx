7aed24c09ac95ada072c1bad9734eb0fc0b33013
Antonio Tosti <5588048+atosti@users.noreply.github.com>
2022-08-07 23:11:14 -0700

    Reorganized sprites, added 8 directional movement, and further expanded on the lore

--- Ruins-of-Aether/Tilemaps/old/my-tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="my-tiles" tilewidth="16" tileheight="16" spacing="1" tilecount="136" columns="17">
- <image source="my-tiles.png" width="288" height="135"/>
-</tileset>
