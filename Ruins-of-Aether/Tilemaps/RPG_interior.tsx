e69ce8dec17136121176731e5096da465019e7f5
Antonio Tosti <5588048+atosti@users.noreply.github.com>
2023-01-20 03:25:23 -0800

    Minor tweaks to the kitchen, assets, and todo

---
+++ Ruins-of-Aether/Tilemaps/RPG_interior.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="RPG_interior" tilewidth="8" tileheight="8" spacing="1" tilecount="209" columns="19">
+ <image source="../assets/sprites/premades/RPG_interior.png" width="176" height="104"/>
+</tileset>
